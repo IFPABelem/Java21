ceae2f81708662355046de366e070ad5c68bbd3b
TiagoDanin <TiagoDanin@outlook.com>
2019-08-25 16:03:46 -0300

    feat: feito questão do vôlei (18)

---
+++ src/ex18/Equipe.java
@@ -0,0 +1,22 @@
+package ex18;
+
+public class Equipe {
+    private String nome;
+    private String cidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
---
+++ src/ex18/Jogador.java
@@ -0,0 +1,22 @@
+package ex18;
+
+public class Jogador {
+    private String nome;
+    private String equipe;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEquipe() {
+        return equipe;
+    }
+
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+}
